a896f61557b0f7b4f5f4513361d75b51db451206
Rudolfosehnem <rudolfo_silva@estudante.sesisenai.org.br>
2025-10-27 10:11:43 -0300

    update- correção css

--- html/dashboard.php
+++ html/dashboard.php
@@ -11,7 +11,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Início - NOS TRILHOS</title>
-  <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
